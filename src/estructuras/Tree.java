87b61a30d827c9c27878753920b6f008f24541ca
Kevin Zhang <kazp058@gmail.com>
2022-01-15 23:57:23 -0500

    Estructura de arbol elaborada. Se agregaron las funciones minimas del arbol.

---
+++ src/estructuras/Tree.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Kevin Zhang <kzhang@espol.edu.ec>
+ */
+public class Tree<E> {
+
+    private TreeNode<E> root;
+    private Comparator cmp;
+
+    public Tree() {
+        this(null);
+    }
+
+    public Tree(TreeNode<E> root) {
+        this.root = root;
+    }
+
+    public TreeNode<E> getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode<E> root) {
+        this.root = root;
+    }
+
+    public List<Tree<E>> getChildren() {
+        return root.getChildren();
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public boolean isLeaf() {
+        return root.getChildren().isEmpty();
+    }
+
+    public void addChildren(E content) {
+        TreeNode node = new TreeNode(content);
+        addChildren(node);
+    }
+
+    public void addChildren(TreeNode node) {
+        addChildren(node, root.getChildren().size() - 1);
+    }
+    
+    public void addChildren(TreeNode node, int i){
+        root.getChildren().add(i, new Tree(node));
+    }
+    
+    public void clear(){
+        this.root = null;
+    }
+    
+    public void clearTree(int i){
+        root.getChildren().remove(i);
+    }
+    
+    public List<TreeNode<E>> inorderTree(){
+        if(this.root == null) return null;
+        
+        List<TreeNode<E>> result = new LinkedList<>();
+        
+        
+        
+        return result;
+    }
+}
